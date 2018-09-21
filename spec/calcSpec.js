864b7e69c24e34c5bf1025b1d610737d3c226ed6
Aiden Harvey <aidenharvey102@gmail.com>
2018-09-21 16:15:07 +0000

    updated to run a calculator method

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,17 +1,27 @@
 describe("Calculator", function() {
-    describe("Additional function", function() {
+    
+    beforeEach(function(){
+        calc = new Calculator();
+    });
+    
+
+    describe("add method", function() {
         it("should take 2 arguments and return the sum of the arguments", function() {
-            expect(addition(2, 2)).toBe(4);
+            calc.add(2);
+            calc.add(2);
+            expect(calc.value).toBe(4);
         });
-        
-        it("should not return 4 if given arguments do not produce a sum of 4", function(){
-           expect(addition(7, 19)).toBe(26); 
+
+        it("should not return 4 if given arguments do not produce a sum of 4", function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
-        
-        it ("should call the alert if either argument is undefined", function(){
-           spyOn(window, "alert");
-           addition("Hello");
-           expect(window.alert).toHaveBeenCalledWith("Both arguments must be a number");
+
+        it("should call the alert if either argument is undefined", function() {
+            spyOn(window, "alert");
+            calc.add("Hello");
+            expect(window.alert).toHaveBeenCalledWith("Argument must be a number");
         });
     });
 });
