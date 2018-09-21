fad1b7d78f724c1853ccc1d9604da0442d38e0fd
Aiden Harvey <aidenharvey102@gmail.com>
2018-09-21 16:57:27 +0000

    age check test challenge

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,12 +1,20 @@
-Calculator = function() {
-    this.value = 0;
-};
-
-Calculator.prototype.add = function(number) {
-    if (typeof(number) == "number") {
-        this.value += number;
+function whatCanIDrink(age) {
+    if (age <= 0) {
+        return String("Sorry. I can’t tell what drink because that age is incorrect!");
+    }
+    else if (age < 14) {
+        return String("Drink Toddy");
+    }
+    else if (age < 18) {
+        return String("Drink Coke");
+    }
+    else if (age < 21) {
+        return String("Drink Beer");
+    }
+    else if (age < 130) {
+        return String("Drink Whiskey");
     }
     else {
-        alert("Argument must be a number");
+        return String("Sorry. I can’t tell what drink because that age is incorrect!");
     }
-};
+}
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,27 +1,36 @@
-describe("Calculator", function() {
+describe("whatCanIDrink", function() {
     
-    beforeEach(function(){
-        calc = new Calculator();
+     beforeEach(function(){
+        ageCheck = new whatCanIDrink();
     });
     
-
-    describe("add method", function() {
-        it("should take 2 arguments and return the sum of the arguments", function() {
-            calc.add(2);
-            calc.add(2);
-            expect(calc.value).toBe(4);
+    describe("Addition function", function() {
+        it("should check age of -10 and return undefined string", function() {
+            expect(whatCanIDrink(-10)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         });
-
-        it("should not return 4 if given arguments do not produce a sum of 4", function() {
-            calc.add(7);
-            calc.add(19);
-            expect(calc.value).toBe(26);
+        
+        it("should check age of 13 and return child string", function() {
+            expect(whatCanIDrink(13)).toBe("Drink Toddy");
         });
-
-        it("should call the alert if either argument is undefined", function() {
-            spyOn(window, "alert");
-            calc.add("Hello");
-            expect(window.alert).toHaveBeenCalledWith("Argument must be a number");
+        
+        it("should check age of 17 and return teenager string", function() {
+            expect(whatCanIDrink(17)).toBe("Drink Coke");
+        });
+        
+        it("should check age of 20 and return young adult string", function() {
+            expect(whatCanIDrink(20)).toBe("Drink Beer");
+        });
+        
+        it("should check age of 35 and return adult string", function() {
+            expect(whatCanIDrink(35)).toBe("Drink Whiskey");
+        });
+        
+        it("should check age of 135 and return undefined string", function() {
+            expect(whatCanIDrink(135)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        
+        it("should check age as not a number and return undefined string", function() {
+            expect(whatCanIDrink("Hello")).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
         });
     });
 });
