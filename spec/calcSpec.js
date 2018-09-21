94f6c9142e996520d7c25b15a973b5549286a51e
Aiden Harvey <aidenharvey102@gmail.com>
2018-09-21 15:40:36 +0000

    added rudimentary test

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,11 @@
 describe("Calculator", function() {
     describe("Additional function", function() {
-        it("Should fail", function() {
-            expect(addition()).toBe(42);
+        it("should take 2 arguments and return the sum of the arguments", function() {
+            expect(addition(2, 2)).toBe(4);
+        });
+        
+        it("should not return 4 if given arguments do not produce a sum of 4", function(){
+           expect(addition(7, 19)).toBe(26); 
         });
     });
 });
