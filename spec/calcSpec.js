1e1e14e1c9bff1b3f271d187076e265da4a30ce3
Aiden Harvey <aidenharvey102@gmail.com>
2018-09-21 15:26:35 +0000

    red test

---
+++ spec/calcSpec.js
@@ -0,0 +1,7 @@
+describe("Calculator", function() {
+    describe("Additional function", function() {
+        it("Should fail", function() {
+            expect(addition()).toBe(42);
+        });
+    });
+});
